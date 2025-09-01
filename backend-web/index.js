53e2a41e5fd14d5e1e297ff89c251d1c3768b57e
YousefAyman125 <Yousefayman125125@gmail.com>
2025-09-01 19:04:09 +0300

    add Swagger UI middleware for API documentation

--- backend-web/index.js
+++ backend-web/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(helmet());
 app.use(cors());
 app.use(morgan('combined'));
